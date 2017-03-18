af0f3cf3f3543f3c8cb60747995f0af535ec5d94
Luke Boyle <luke.boyle.au@gmail.com>
2017-03-18 13:01:57 +1100

    refactor: add delete and material card

--- src/js/components/user-debts.js
+++ src/js/components/user-debts.js
@@ -2,58 +2,57 @@ import textFieldToggle from './text-field-toggle';
 
 export default {
 	template: `
-		<div class="columns is-multiline">
-			<div v-if="debts.length !== 0" v-for="debt in debts" class="column is-one-third">
-				<div class="card">
-					<div class="card-content">
-						<text-field-toggle 
-							label="Debt Name" 
-							type="text"
-							placeholder="Enter the name of your debt"
-							property="name"
-							:debt="debt"
-							:value="debt.name"
-							:handle-value-changed="handleValueChanged"
-						></text-field-toggle>
-						<text-field-toggle 
-							label="Debt Amount"
-							type="number"
-							placeholder="Enter the amount left in the debt"
-							property="amount"
-							:debt="debt"
-							:value="debt.amount"
-							:handle-value-changed="handleValueChanged"
-						></text-field-toggle>
-						<text-field-toggle 
-							label="Annual Percentage Rate"
-							type="number"
-							placeholder="Enter the APR of the debt"
-							property="interest"
-							:debt="debt"
-							:value="debt.interest"
-							:handle-value-changed="handleValueChanged"
-						></text-field-toggle>
-						<text-field-toggle 
-							label="Minimum Monthly Repayment" 
-							type="number"
-							placeholder="Enter your monthly repayment"
-							property="minPayment"
-							:debt="debt"
-							:value="debt.minPayment"
-							:handle-value-changed="handleValueChanged"
-						></text-field-toggle>
-					</div>
-				</div>
-			</div>
-			<div class="column is-one-third">
-				<div class="card">
-					<div class="card-content">
-						<button class="button" @click="handleNewDebtButtonPressed">
-							&plus;
-						</button>
-					</div>
-				</div>
-			</div>
+		<div class="flex-grid" v-if="debts.length !== 0" >
+			<md-card v-for="debt in debts" class="user-debt" key="debt.id">
+				<md-card-header>
+					<md-card-header-text>
+						<div class="md-title">{{ debt.name }}</div>
+					</md-card-header-text>
+					<md-button class="md-icon-button" @click.native="handleDeleteDebtButtonPressed(debt.id)">
+						<md-icon>delete</md-icon>
+					</md-button>
+				</md-card-header>
+				<md-card-content>
+					<text-field-toggle 
+						label="Debt Name" 
+						type="text"
+						placeholder="Enter the name of your debt"
+						property="name"
+						:debt="debt"
+						:value="debt.name"
+						:handle-value-changed="handleValueChanged"
+					></text-field-toggle>
+					<text-field-toggle 
+						label="Debt Amount"
+						type="number"
+						placeholder="Enter the amount left in the debt"
+						property="amount"
+						:debt="debt"
+						:value="debt.amount"
+						:handle-value-changed="handleValueChanged"
+					></text-field-toggle>
+					<text-field-toggle 
+						label="Annual Percentage Rate"
+						type="number"
+						placeholder="Enter the APR of the debt"
+						property="interest"
+						:debt="debt"
+						:value="debt.interest"
+						:handle-value-changed="handleValueChanged"
+					></text-field-toggle>
+					<text-field-toggle 
+						label="Minimum Monthly Repayment" 
+						type="number"
+						placeholder="Enter your monthly repayment"
+						property="minPayment"
+						:debt="debt"
+						:value="debt.minPayment"
+						:handle-value-changed="handleValueChanged"
+					></text-field-toggle>
+				</md-card-content>
+			</md-card>
+			<div class="user-debt"></div>
+			<div class="user-debt"></div>
 		</div>
 	`,
 	components: {
@@ -61,7 +60,7 @@ export default {
 	},
 	props: {
 		handleValueChanged: Function,
-		handleNewDebtButtonPressed: Function,
+		handleDeleteDebtButtonPressed: Function,
 		editMode: Boolean,
 		debts: Array
 	}

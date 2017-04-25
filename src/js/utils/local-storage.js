dfe5f7449ebb6ee8959ce2ff4e8b8c29f1b7b586
Luke Boyle <luke.boyle.au@gmail.com>
2017-04-26 00:39:05 +1000

    fix: local storage function double encoding

--- src/js/utils/local-storage.js
+++ src/js/utils/local-storage.js
@@ -1,25 +1,26 @@
 import {defaultUserData} from './constants';
 import locationManager from './location-manager';
+import { Base64 } from 'js-base64';
 
 export function updateLocalUserData(keyToChange, dataToChange) {
 	if (keyToChange === 'userData') {
-		const data = JSON.stringify(dataToChange);
-		locationManager.hash('userData', btoa(data));
+		locationManager.hash('userData', Base64.encode(JSON.stringify(dataToChange)));
 	} else {
 		const data = JSON.stringify({
 			...getUserData(),
 			[keyToChange]: dataToChange
 		});
-		locationManager.hash('userData', btoa(data));
+		locationManager.hash('userData', Base64.encode(data));
 	}
 }
 
 export function getUserData() {
 	const encodedUserData = locationManager.hash('userData');
-	if (encodedUserData) {
-		return JSON.parse(atob(encodedUserData));
+	if (!!encodedUserData) {
+		console.log(JSON.parse(Base64.decode(encodedUserData)));
+		return JSON.parse(Base64.decode(encodedUserData));
 	} else {
-		updateLocalUserData('userData', btoa(JSON.stringify(defaultUserData)));
+		updateLocalUserData('userData', defaultUserData);
 		return defaultUserData;
 	}
 }

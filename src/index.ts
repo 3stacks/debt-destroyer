22942e52c574eadd4f4ce14c50adffdfa27f1d15
Luke Boyle <me@lukeboyle.com>
2019-08-22 23:47:04 +1000

    fix: add do not track opt out

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import './index.css';
 
+if (window.navigator.doNotTrack) {
+	(window as any).mixpanel.opt_out_tracking();
+}
+
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
